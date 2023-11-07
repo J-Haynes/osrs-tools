2a8db5a9df22485fd9e4464f6697d8b355ec9f9b
Jack Haynes <haynes.jack.stewart@gmail.com>
2023-11-08 01:00:29 +1300

    combines topbar and sidebar into one component, sidebar is now responsive

--- src/App.tsx
+++ src/App.tsx
@@ -2,36 +2,23 @@ import React, { useState, useEffect } from 'react'
 import MenuIcon from '@mui/icons-material/Menu'
 import './App.css'
 
-import Sidebar from './components/Sidebar'
+import Nav from './components/Nav'
 
 import { ThemeOptions } from '@mui/material/styles'
 import { ThemeProvider } from '@emotion/react'
 
 import { createTheme } from '@mui/material/styles'
 import { themeOptions } from './themeSettings'
-import TopBar from './components/TopBar'
 import { Box, useMediaQuery } from '@mui/material'
 
 const theme = createTheme(themeOptions)
 
 function App() {
-  const isScreenSmall = useMediaQuery('(max-width: 600px)')
-  const [sidebarOpen, setSidebarOpen] = useState(!isScreenSmall)
-
-  const toggleSidebar = () => {
-    setSidebarOpen(!sidebarOpen)
-  }
-
-  useEffect(() => {
-    setSidebarOpen(!isScreenSmall)
-  }, [isScreenSmall])
-
   return (
     <>
       <ThemeProvider theme={theme}>
         <Box sx={{ display: 'flex' }}>
-          <TopBar toggle={toggleSidebar} />
-          <Sidebar open={sidebarOpen} onClose={toggleSidebar} />
+          <Nav />
         </Box>
       </ThemeProvider>
     </>
---
+++ src/components/Nav.tsx
@@ -0,0 +1,96 @@
+import React, { useState } from 'react'
+import Drawer from '@mui/material/Drawer'
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import ListItemText from '@mui/material/ListItemText'
+import MenuIcon from '@mui/icons-material/Menu'
+import { Typography } from '@mui/material'
+
+import { styled } from '@mui/system'
+import {
+  AppBar,
+  Divider,
+  IconButton,
+  ListItemButton,
+  ListItemIcon,
+  Toolbar,
+} from '@mui/material'
+
+import Leaderboard from '@mui/icons-material/Leaderboard'
+
+const drawerWidth = 240
+
+const drawer = (
+  <div>
+    <List>
+      <ListItemButton>
+        <ListItemText primary="Home" />
+      </ListItemButton>
+      <Divider />
+      <ListItemButton>
+        <ListItemIcon>
+          <Leaderboard />
+        </ListItemIcon>
+        <ListItemText primary="Combat Calculator" />
+      </ListItemButton>
+    </List>
+  </div>
+)
+
+export default function Nav() {
+  const [mobileOpen, setMobileOpen] = React.useState(false)
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen)
+  }
+  return (
+    <>
+      <AppBar
+        position="fixed"
+        sx={{
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+          ml: { sm: `${drawerWidth}px` },
+        }}
+      >
+        <Toolbar style={{ display: 'flex' }}>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            onClick={handleDrawerToggle}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography align="center" noWrap component="div">
+            Test here
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        // container={container}
+        variant="temporary"
+        open={mobileOpen}
+        onClose={handleDrawerToggle}
+        ModalProps={{
+          keepMounted: true, // Better open performance on mobile.
+        }}
+        sx={{
+          display: { xs: 'block', sm: 'none' },
+          '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+        }}
+      >
+        {drawer}
+      </Drawer>
+      <Drawer
+        variant="permanent"
+        sx={{
+          display: { xs: 'none', sm: 'block' },
+          '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+        }}
+        open
+      >
+        {drawer}
+      </Drawer>
+    </>
+  )
+}
