407ee5d1257e3802be2e17ea6cac6b0116d9543b
Jack Haynes <haynes.jack.stewart@gmail.com>
2023-12-05 23:37:07 +1300

    installs cheerio, world tracker component now has button which fetches world populations

---
+++ server/server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const cors = require('cors')
+const axios = require('axios')
+
+const app = express()
+const port = 4000
+
+app.use(express.json())
+
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+}
+
+app.use(cors(corsOptions))
+
+app.get('/proxy', async (req, res) => {
+  try {
+    const response = await axios.get(
+      'https://oldschool.runescape.com/slu?order=WMLPA'
+    )
+    res.json(response.data)
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching data' })
+  }
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`)
+})
--- src/components/WorldTracker.tsx
+++ src/components/WorldTracker.tsx
@@ -2,12 +2,26 @@ import React from 'react'
 import { Box, Button } from '@mui/material'
 
 import * as cheerio from 'cheerio'
+import axios from 'axios'
 
 export default function WorldTracker() {
-  const $ = cheerio.load('https://oldschool.runescape.com/slu')
+  const worldTracker = async () => {
+    try {
+      const response = await axios.get('http://localhost:4000/proxy')
 
-  const worldTracker = () => {
-    console.log($('table.tbody'))
+      const $ = cheerio.load(response.data)
+
+      const dataObject = {} as any
+      $('table tr').each((index, element) => {
+        const columns = $(element).find('td')
+        const key = $(columns[0]).text().trim()
+        const value = $(columns[1]).text().trim()
+        dataObject[key] = value
+      })
+      console.log('Data Object:', dataObject)
+    } catch (error) {
+      console.error('Error fetching', error)
+    }
   }
 
   return (
