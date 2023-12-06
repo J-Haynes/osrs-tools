c2fb9d75460171a5fb6273e6de8e0fc0221ef852
Jack Haynes <haynes.jack.stewart@gmail.com>
2023-12-07 01:22:43 +1300

    changes to arrays starts loop

--- src/api/clientApi.ts
+++ src/api/clientApi.ts
@@ -10,18 +10,20 @@ export const fetchWorlds = async () => {
     const dataObject = {} as any
     $('table tr').each((index, element) => {
       const columns = $(element).find('td')
-      const key = Number(
-        $(columns[0])
-          .text()
-          .trim()
-          .replace('Old School ', '')
-          .replace('OldSchool ', '')
-      )
+      const key =
+        Number(
+          $(columns[0])
+            .text()
+            .trim()
+            .replace('Old School ', '')
+            .replace('OldSchool ', '')
+        ) + 300
       const value = Number($(columns[1]).text().trim().replace(' players', ''))
       dataObject[key] = value
     })
-    delete dataObject[0]
+    delete dataObject[300]
     console.log(dataObject)
+    return dataObject
   } catch (error) {
     console.error('Error fetching', error)
   }
